d294ed7bd4b6c491e651aed4361444156f49d772
Payal Yeole <yeolepayal366@gmail.com>
2025-10-28 11:07:47 +0530

    Added Bill entity

--- src/main/java/com/hms/HospitalManagementSystem/models/Bill.java
+++ src/main/java/com/hms/HospitalManagementSystem/models/Bill.java
@@ -1,2 +1,24 @@
-package com.hms.HospitalManagementSystem.models;public class Bill {
+package com.hms.HospitalManagementSystem.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Bill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private long patientId;
+    private double amount;
+    private String status;
 }
