3bbb1d1750a323e9fd300560ba2bc24f9eb451df
Payal Yeole <yeolepayal366@gmail.com>
2025-10-28 11:10:49 +0530

    Added Appointment entity

--- src/main/java/com/hms/HospitalManagementSystem/models/Appointment.java
+++ src/main/java/com/hms/HospitalManagementSystem/models/Appointment.java
@@ -1,2 +1,23 @@
-package com.hms.HospitalManagementSystem.models;public class Appointment {
+package com.hms.HospitalManagementSystem.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Appointment {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String Name;
+    private String gender;
+    private int age;
 }
