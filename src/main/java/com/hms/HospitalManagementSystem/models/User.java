32cd68dab671337d9f79dd1e94d235df4de9d089
Payal Yeole <yeolepayal366@gmail.com>
2026-05-14 17:32:24 +0530

    Added user file to added user

---
+++ src/main/java/com/hms/HospitalManagementSystem/models/User.java
@@ -0,0 +1,25 @@
+package com.hms.HospitalManagementSystem.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role = "USER";
+}
